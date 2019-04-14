ffacd772e003b9a6c03d77f742d63e89ffc39aff
Alok Bhowmik <38933053+alokbhowmik@users.noreply.github.com>
2019-04-14 16:00:32 +0530

    Update LoveCal.java

--- LoveCal.java
+++ LoveCal.java
@@ -4,7 +4,7 @@ import java.awt.event.*;
 class LoginFrame extends JFrame implements ActionListener
 {
    Container c;
-     JLabel cal=new JLabel("LOVE CALCULATOR");
+     JLabel cal=new JLabel("              LOVE CALCULATOR");
    JLabel userlabel=new JLabel("ENTER YOUR NAME");
    JLabel passlabel=new JLabel("ENTER NAME OF YOUR  LOVER");
    JTextField usertf=new JTextField();
@@ -12,20 +12,26 @@ class LoginFrame extends JFrame implements ActionListener
 	JButton login=new JButton("RESULT");
 	JLabel percentage=new JLabel("PERCENTAGES");
 	JTextField pertf=new JTextField();
-	 JLabel label=new JLabel("your score is");
+	 JLabel label=new JLabel("");
 	LoginFrame()
 	 {
 	    c=this.getContentPane();
 		c.setLayout(null);
 		c.setBackground(Color.YELLOW);
-		cal.setBounds(200,20,200,30);
+		cal.setBounds(200,20,350,30);
 		userlabel.setBounds(100,70,400,40);
 		passlabel.setBounds(100,120,400,40);
 		usertf.setBounds(450,70,150,40);
 		passtf.setBounds(450,120,150,40);
 		login.setBounds(200,250,200,50);
-		percentage.setBounds(200,300,250,50);
+        percentage.setBounds(200,300,250,50);
+        // pertf.setEnabled(false);
+        pertf.setForeground(Color.BLACK);
+        pertf.setFont(new Font("Algerian",Font.BOLD,20));
 		pertf.setBounds(200,350,200,40);
+        label.setBounds(40,400,700,40);
+        label.setFont(new Font("Pacifico",Font.BOLD,20));
+        label.setForeground(Color.RED);
 		Font f=new Font("Arial",Font.BOLD,20);
 		cal.setFont(f);
 		userlabel.setFont(f);
@@ -55,16 +61,18 @@ class LoginFrame extends JFrame implements ActionListener
 			 int sum1=0,sum2=0;
 			 for(int i=0;i<username.length();i++){
 				 char ch=username.charAt(i);
-				 int ascii=ch;
+				 int ascii=(int)ch;
 				 sum1=sum1+ascii;
 			 }
 			  for(int i=0;i<passname.length();i++){
 				 char ch=passname.charAt(i);
-				 int ascii=ch;
+				 int ascii=(int)ch;
 				 sum2=sum2+ascii;
 			 }
-			 int total=sum1+sum2;
-			 int love=total/100;
+             int total=sum1+sum2;
+             System.out.println(total);
+             int love=total%100;
+            // int love =80;
 			 pertf.setText(Integer.toString(love)+"%");
 			 if(love>75)
 			 {
@@ -87,6 +95,6 @@ class LoveCal
 		f.setTitle("LOVE CALCULATOR");
 		f.setVisible(true);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.setBounds(100,50,200,500);
+		f.setBounds(100,50,770,500);
 	}
 }
